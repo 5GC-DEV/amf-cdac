44aebcf9f47819508d10985bb389b29a848e8601
AshithaCDAC <ashitha@cdac.in>
2024-08-28 14:22:53 +0530

    ksi value updated after 5G-AKA

--- gmm/handler.go
+++ gmm/handler.go
@@ -1558,6 +1558,17 @@ func AuthenticationProcedure(ue *context.AmfUe, accessType models.AccessType) (b
 	ue.AuthenticationCtx = response
 	ue.ABBA = []uint8{0x00, 0x00} // set ABBA value as described at TS 33.501 Annex A.7.1
 
+	// testing if it is working
+	if ue.NgKsi.Tsc == models.ScType_NATIVE && ue.NgKsi.Ksi != 7 {
+		// Modification by CDAC TVM as per the Specification 24.501 - 5.4.1.3.2 Authentication initiation by the network
+		// select new ngksi
+		if ue.NgKsi.Ksi < 6 { // ksi is range from 0 to 6
+			ue.NgKsi.Ksi += 1
+		} else {
+			ue.NgKsi.Ksi = 0
+		}
+	}
+	ue.GmmLog.Info("---value of ksi after 5g-aka: ", ue.NgKsi.Ksi)
 	gmm_message.SendAuthenticationRequest(ue.RanUe[accessType])
 	return false, nil
 }
