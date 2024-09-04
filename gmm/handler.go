b649c99df85dcf8ef4891c5f282a33a0b6c5a2d4
AshithaCDAC <ashitha@cdac.in>
2024-09-04 18:14:28 +0530

    logs added sync auth vector

--- gmm/handler.go
+++ gmm/handler.go
@@ -389,6 +389,8 @@ func HandleRegistrationRequest(ue *context.AmfUe, anType models.AccessType, proc
 	if ue == nil {
 		return fmt.Errorf("AmfUe is nil")
 	}
+	ue.GmmLog.Info("---ue.ULCount: ", ue.ULCount)
+	ue.GmmLog.Info("---ue.AuthenticationCtx: ", ue.AuthenticationCtx)
 
 	ue.GmmLog.Info("Handle Registration Request")
 
