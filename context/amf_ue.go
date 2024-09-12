137d8fe792ae4b646e321f19e26328e47d3643b5
AshithaCDAC <ashitha@cdac.in>
2024-09-12 18:08:20 +0530

    sd value nil case fix in sec mode slice handling

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -557,12 +557,13 @@ func (ue *AmfUe) InAllowedNssai(targetSNssai models.Snssai, anType models.Access
 }
 
 func (ue *AmfUe) InSubscribedNssai(targetSNssai models.Snssai) bool {
-	logger.ContextLog.Debug("ue.SubscribedNssai", ue.SubscribedNssai)
+	logger.ContextLog.Debug("ue.SubscribedNssai: ", ue.SubscribedNssai)
 	for _, sNssai := range ue.SubscribedNssai {
 		sst := sNssai.SubscribedSnssai.Sst
 		sd := sNssai.SubscribedSnssai.Sd
 		logger.ContextLog.Debug("sst: ", sst)
 		logger.ContextLog.Debug("sd: ", sd)
+		// SD nil case - if SD value is nil checking SSt values only
 		if targetSNssai.Sd != "" {
 			if reflect.DeepEqual(*sNssai.SubscribedSnssai, targetSNssai) {
 				return true
