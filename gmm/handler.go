4902a04b123fb5206a103924c58406a56a67a34c
AshithaCDAC <ashitha@cdac.in>
2024-11-22 18:06:44 +0530

    Unwanted logs removed

--- gmm/handler.go
+++ gmm/handler.go
@@ -1230,8 +1230,6 @@ func handleRequestedNssai(ue *context.AmfUe, anType models.AccessType) error {
 				}
 				ue.AllowedNssai[anType] = append(ue.AllowedNssai[anType], allowedSnssai)
 				ue.GmmLog.Debug("allowedSnssai: ", allowedSnssai)
-				ue.GmmLog.Info("---allowedSnssai sst: ", allowedSnssai.AllowedSnssai.Sst)
-				ue.GmmLog.Info("---allowedSnssai sd: ", allowedSnssai.AllowedSnssai.Sd)
 				ue.GmmLog.Info("slices are identical")
 				disableSliceSelection = true
 				break
