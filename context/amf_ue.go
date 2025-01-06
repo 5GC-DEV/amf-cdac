148e76f35ed0348eeb37c7cd0fadd509df1797d2
anaswara <anaswara.n@cdac.in>
2025-01-06 15:53:18 +0530

    oldran-ue commented

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -508,10 +508,10 @@ func (ue *AmfUe) AttachRanUe(ranUe *RanUe) {
 	/* detach any RanUe associated to it */
 	oldRanUe := ue.RanUe[ranUe.Ran.AnType]
 	ue.RanUe[ranUe.Ran.AnType] = ranUe
-	ranUe.AmfUe = ue
 	if ue == nil {
 		logger.NgapLog.Warnf(" -----  AmfUe is nil at this point.")
 	}
+	ranUe.AmfUe = ue
 
 	go func() {
 		time.Sleep(time.Second * 2)
@@ -519,7 +519,7 @@ func (ue *AmfUe) AttachRanUe(ranUe *RanUe) {
 			// nilcheck oldranue.log - cdac
 			if oldRanUe.Log != nil {
 				oldRanUe.Log.Infof("Detached UeContext from OldRanUe")
-				oldRanUe.AmfUe = nil
+				//oldRanUe.AmfUe = nil
 			}
 		}
 	}()
@@ -528,7 +528,7 @@ func (ue *AmfUe) AttachRanUe(ranUe *RanUe) {
 	ue.NASLog = logger.NasLog.With(logger.FieldAmfUeNgapID, fmt.Sprintf("AMF_UE_NGAP_ID:%d", ranUe.AmfUeNgapId))
 	ue.GmmLog = logger.GmmLog.With(logger.FieldAmfUeNgapID, fmt.Sprintf("AMF_UE_NGAP_ID:%d", ranUe.AmfUeNgapId))
 	ue.TxLog = logger.GmmLog.With(logger.FieldAmfUeNgapID, fmt.Sprintf("AMF_UE_NGAP_ID:%d", ranUe.AmfUeNgapId))
-	logger.NgapLog.Infof("Attachment process completed for AmfUe with ID: %d and RanUeNgapId: %d", ue.Supi, ranUe.RanUeNgapId)
+	logger.NgapLog.Infof("-----------  Attachment process completed for AmfUe with ID: %d and RanUeNgapId: %d", ue.Supi, ranUe.RanUeNgapId)
 }
 
 func (ue *AmfUe) GetAnType() models.AccessType {
