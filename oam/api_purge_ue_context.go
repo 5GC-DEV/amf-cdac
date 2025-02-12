0a7a0fd54b78f11dd44a7999f1e1f2b21997543d
AshithaCDAC <ashitha@cdac.in>
2025-02-12 19:22:52 +0530

    package redeclaration error fix

--- oam/api_purge_ue_context.go
+++ oam/api_purge_ue_context.go
@@ -15,8 +15,6 @@ import (
 	"github.com/omec-project/openapi/models"
 	"github.com/omec-project/util/httpwrapper"
 
-	"github.com/omec-project/openapi/models"
-
 )
 
 func HTTPPurgeUEContext(c *gin.Context) {
