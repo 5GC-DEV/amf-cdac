beed00248114f1010e3b45f3fe253907160d6e27
AshithaCDAC <ashitha@cdac.in>
2025-09-24 12:57:16 +0000

    removed global lock

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -67,8 +67,10 @@ const (
 )
 
 type AmfUe struct {
-	// Mutex sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
-	Mutex sync.RWMutex `json:"-"`
+	Mutex       sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
+	StateMu     sync.RWMutex
+	SmctxlistMu sync.RWMutex
+	// Mutex sync.RWMutex `json:"-"`
 	/* the AMF which serving this AmfUe now */
 	ServingAMF *AMFContext `json:"servingAMF,omitempty"` // never nil
 
@@ -230,11 +232,11 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 			amfUeNgapIDVal = ue.RanUe[models.AccessType__3_GPP_ACCESS].AmfUeNgapId
 		}
 	}
-	ue.Mutex.RLock()
+	ue.StateMu.RLock()
 	for access, state := range ue.State {
 		stateVal[access] = string(state.Current())
 	}
-	ue.Mutex.RUnlock()
+	ue.StateMu.RUnlock()
 	n1n2MsgVal := N1N2Message{}
 	if ue.N1N2Message != nil {
 		n1n2MsgVal = *ue.N1N2Message
@@ -269,9 +271,9 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 		newSmCtx.SetNsInstance(smContext.NsInstance())
 
 		pduSessIdStr := strconv.FormatInt(int64(pduSessId), 10)
-		ue.Mutex.RLock()
+		ue.SmctxlistMu.RLock()
 		smCtxListVal[pduSessIdStr] = *newSmCtx
-		ue.Mutex.RUnlock()
+		ue.SmctxlistMu.RUnlock()
 		return true
 	})
 
@@ -313,7 +315,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 	if !ok {
 		logger.ContextLog.Warnln("Ran Connection is not Exist with GnbID: ", aux.RanId)
 	}
-	ue.Mutex.RLock()
+	ue.StateMu.RLock()
 	for index, states := range aux.State {
 		ue.State[index] = fsm.NewState(fsm.StateType(states))
 		if ue.RanUe[index] == nil {
@@ -327,7 +329,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 			ue.RanUe[index].Ran = ran
 		}
 	}
-	ue.Mutex.RUnlock()
+	ue.StateMu.RUnlock()
 	for key, val := range aux.SmCtxList {
 		keyVal, err := strconv.ParseInt(key, 10, 32)
 		if err != nil {
