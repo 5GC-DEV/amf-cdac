2bbf31a2e6a7a2494d44ad0e41a0ef30660bb772
AshithaCDAC <ashitha@cdac.in>
2025-09-30 13:18:22 +0000

    logs modified

--- ngap/dispatcher.go
+++ ngap/dispatcher.go
@@ -137,10 +137,12 @@ func Dispatch(conn net.Conn, msg []byte) {
 	}
 
 	ranUe, _ := FetchRanUeContext(ran, pdu)
-	ran.Log.Info("ranue: ", ranUe)
-	ran.Log.Info("ranue amfue: ", ranUe.AmfUe)
-	ran.Log.Info("ranue amfuengapid: ", ranUe.AmfUeNgapId)
-	ran.Log.Info("ranue ranuengapid: ", ranUe.RanUeNgapId)
+	if ranUe != nil {
+		ran.Log.Info("ranue: ", ranUe)
+		ran.Log.Info("ranue amfue: ", ranUe.AmfUe)
+		ran.Log.Info("ranue amfuengapid: ", ranUe.AmfUeNgapId)
+		ran.Log.Info("ranue ranuengapid: ", ranUe.RanUeNgapId)
+	}
 	if ranUe.Ran != nil {
 		ran.Log.Info("ranue: ", ranUe.Ran)
 	}
