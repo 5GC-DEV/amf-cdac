a97c1ab8eefcad74e6654251746456d92a7e08d7
anaswara <anaswara.n@cdac.in>
2025-12-04 12:00:23 +0530

    commented

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -579,10 +579,10 @@ func (ue *AmfUe) InSubscribedNssai(targetSNssai *models.Snssai) bool {
 		logger.ContextLog.Info("sst:", targetSNssai.Sst, ", sd:", targetSNssai.Sd, "from gNB")
 		if reflect.DeepEqual(*sNssai.SubscribedSnssai, targetSNssai) {
 			return true
-		} else if sNssai.SubscribedSnssai.Sst == targetSNssai.Sst {
+		} /*else if sNssai.SubscribedSnssai.Sst == targetSNssai.Sst {
 			targetSNssai.Sd = sNssai.SubscribedSnssai.Sd
 			return true
-		}
+		}*/
 	}
 	return false
 }
