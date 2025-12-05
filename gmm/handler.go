09352ffb74fee495c5e4726156d20e6480e2d68e
anaswara <anaswara.n@cdac.in>
2025-12-05 14:31:42 +0530

    updated

--- gmm/handler.go
+++ gmm/handler.go
@@ -1229,7 +1229,7 @@ func getSubscribedNssai(ue *context.AmfUe) {
 // TS 23.502 4.2.2.2.3 Registration with AMF Re-allocation
 func handleRequestedNssai(ue *context.AmfUe, anType models.AccessType) error {
 	amfSelf := context.AMF_Self()
-	disableSliceSelection := true
+	// disableSliceSelection := false
 
 	if ue.RegistrationRequest.RequestedNSSAI != nil {
 		requestedNssai, err := nasConvert.RequestedNssaiToModels(ue.RegistrationRequest.RequestedNSSAI)
@@ -1240,7 +1240,7 @@ func handleRequestedNssai(ue *context.AmfUe, anType models.AccessType) error {
 		ue.GmmLog.Infof("RequestedNssai: %+v", requestedNssai)
 
 		needSliceSelection := false
-		disableSliceSelection = false
+		disableSliceSelection := false
 		for _, requestedSnssai := range requestedNssai {
 			ue.GmmLog.Debug("reg req Sst: ", requestedSnssai.ServingSnssai.Sst)
 			ue.GmmLog.Debug("reg req Sd: ", requestedSnssai.ServingSnssai.Sd)
