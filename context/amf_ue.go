b72f8ce954cb043f57ea5b961b7b9042ab4f9a16
AshithaCDAC <ashitha@cdac.in>
2026-01-23 12:29:35 +0000

    lock added for ue and removed others

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -67,9 +67,9 @@ const (
 )
 
 type AmfUe struct {
-	Mutex       sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
-	StateMu     sync.RWMutex
-	SmctxlistMu sync.RWMutex
+	Mutex sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
+	// StateMu     sync.RWMutex
+	// SmctxlistMu sync.RWMutex
 	// Mutex sync.RWMutex `json:"-"`
 	/* the AMF which serving this AmfUe now */
 	ServingAMF *AMFContext `json:"servingAMF,omitempty"` // never nil
@@ -220,6 +220,8 @@ type AmfUe struct {
 }
 
 func (ue *AmfUe) MarshalJSON() ([]byte, error) {
+	ue.Mutex.Lock()
+	defer ue.Mutex.Unlock()
 	type Alias AmfUe
 	stateVal := make(map[models.AccessType]string)
 	smCtxListVal := make(map[string]SmContext)
@@ -232,11 +234,11 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 			amfUeNgapIDVal = ue.RanUe[models.AccessType__3_GPP_ACCESS].AmfUeNgapId
 		}
 	}
-	ue.StateMu.RLock()
+
 	for access, state := range ue.State {
 		stateVal[access] = string(state.Current())
 	}
-	ue.StateMu.RUnlock()
+
 	n1n2MsgVal := N1N2Message{}
 	if ue.N1N2Message != nil {
 		n1n2MsgVal = *ue.N1N2Message
@@ -271,9 +273,9 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 		newSmCtx.SetNsInstance(smContext.NsInstance())
 
 		pduSessIdStr := strconv.FormatInt(int64(pduSessId), 10)
-		ue.SmctxlistMu.RLock()
+
 		smCtxListVal[pduSessIdStr] = *newSmCtx
-		ue.SmctxlistMu.RUnlock()
+
 		return true
 	})
 
@@ -317,7 +319,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 	if !ok {
 		logger.ContextLog.Warnln("Ran Connection is not Exist with GnbID: ", aux.RanId)
 	}
-	ue.StateMu.Lock()
+
 	for index, states := range aux.State {
 		ue.State[index] = fsm.NewState(fsm.StateType(states))
 		if ue.RanUe[index] == nil {
@@ -331,7 +333,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 			ue.RanUe[index].Ran = ran
 		}
 	}
-	ue.StateMu.Unlock()
+
 	for key, val := range aux.SmCtxList {
 		keyVal, err := strconv.ParseInt(key, 10, 32)
 		if err != nil {
