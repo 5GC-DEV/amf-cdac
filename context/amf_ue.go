953b85f2e840117c5cf58dcc0d57a73c51913c9b
AshithaCDAC <ashitha@cdac.in>
2026-02-05 15:30:26 +0000

    replaced lock with rlock in marshaljson

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -67,7 +67,8 @@ const (
 )
 
 type AmfUe struct {
-	Mutex sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
+	Mutex sync.RWMutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
+
 	// StateMu     sync.RWMutex
 	// SmctxlistMu sync.RWMutex
 	// Mutex sync.RWMutex `json:"-"`
@@ -220,8 +221,8 @@ type AmfUe struct {
 }
 
 func (ue *AmfUe) MarshalJSON() ([]byte, error) {
-	ue.Mutex.Lock()
-	defer ue.Mutex.Unlock()
+	ue.Mutex.RLock()
+	defer ue.Mutex.RUnlock()
 	type Alias AmfUe
 	stateVal := make(map[models.AccessType]string)
 	smCtxListVal := make(map[string]SmContext)
