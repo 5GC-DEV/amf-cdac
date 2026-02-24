382e9805334924b550f562f82967cfba0782dd74
AshithaCDAC <ashitha@cdac.in>
2026-02-24 06:29:54 +0000

    lock added before marshal function

--- context/amf_ue.go
+++ context/amf_ue.go
@@ -67,9 +67,9 @@ const (
 )
 
 type AmfUe struct {
-	Mutex       sync.Mutex `json:"mutex,omitempty" yaml:"mutex" bson:"mutex,omitempty"`
-	StateMu     sync.RWMutex
-	SmctxlistMu sync.RWMutex
+	Mutex sync.RWMutex
+	// StateMu     sync.RWMutex
+	// SmctxlistMu sync.RWMutex
 	// Mutex sync.RWMutex `json:"-"`
 	/* the AMF which serving this AmfUe now */
 	ServingAMF *AMFContext `json:"servingAMF,omitempty"` // never nil
@@ -233,11 +233,11 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 			amfUeNgapIDVal = ue.RanUe[models.AccessType__3_GPP_ACCESS].AmfUeNgapId
 		}
 	}
-	ue.StateMu.RLock()
+	// ue.StateMu.RLock()
 	for access, state := range ue.State {
 		stateVal[access] = string(state.Current())
 	}
-	ue.StateMu.RUnlock()
+	// ue.StateMu.RUnlock()
 	n1n2MsgVal := N1N2Message{}
 	if ue.N1N2Message != nil {
 		n1n2MsgVal = *ue.N1N2Message
@@ -272,9 +272,9 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 		newSmCtx.SetNsInstance(smContext.NsInstance())
 
 		pduSessIdStr := strconv.FormatInt(int64(pduSessId), 10)
-		ue.SmctxlistMu.RLock()
+		// ue.SmctxlistMu.RLock()
 		smCtxListVal[pduSessIdStr] = *newSmCtx
-		ue.SmctxlistMu.RUnlock()
+		// ue.SmctxlistMu.RUnlock()
 		return true
 	})
 
@@ -288,7 +288,8 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 		N1N2Message: n1n2MsgVal,
 		RanId:       gnbId,
 	}
-
+	ue.Mutex.RLock()
+	defer ue.Mutex.RUnlock()
 	return json.Marshal(&struct {
 		CustomAmfUe CustomFieldsAmfUe `json:"customFieldsAmfUe"`
 		*Alias
@@ -299,6 +300,8 @@ func (ue *AmfUe) MarshalJSON() ([]byte, error) {
 }
 
 func (ue *AmfUe) UnmarshalJSON(data []byte) error {
+	ue.Mutex.Lock()
+	defer ue.Mutex.Unlock()
 	type Alias AmfUe
 	auxCustom := &struct {
 		CustomAmfUe CustomFieldsAmfUe `json:"customFieldsAmfUe"`
@@ -316,7 +319,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 	if !ok {
 		logger.ContextLog.Warnln("Ran Connection is not Exist with GnbID: ", aux.RanId)
 	}
-	ue.StateMu.RLock()
+	// ue.StateMu.RLock()
 	for index, states := range aux.State {
 		ue.State[index] = fsm.NewState(fsm.StateType(states))
 		if ue.RanUe[index] == nil {
@@ -330,7 +333,7 @@ func (ue *AmfUe) UnmarshalJSON(data []byte) error {
 			ue.RanUe[index].Ran = ran
 		}
 	}
-	ue.StateMu.RUnlock()
+	// ue.StateMu.RUnlock()
 	for key, val := range aux.SmCtxList {
 		keyVal, err := strconv.ParseInt(key, 10, 32)
 		if err != nil {
