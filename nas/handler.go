b564989a9ab7f77f72454f3f7772220b8d440a8f
AshithaCDAC <ashitha@cdac.in>
2026-05-08 09:59:46 +0000

    fix of deadlock

--- nas/handler.go
+++ nas/handler.go
@@ -23,8 +23,6 @@ func HandleNAS(ue *context.RanUe, procedureCode int64, nasPdu []byte) {
 		logger.NasLog.Errorln("RanUe is nil")
 		return
 	}
-	ue.AmfUe.Mutex.Lock()
-	defer ue.AmfUe.Mutex.Unlock()
 
 	if nasPdu == nil {
 		ue.Log.Errorln("nasPdu is nil")
