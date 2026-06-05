3a5ed1165a86f64bf49f56c68792db2ca170f72a
AshithaCDAC <ashitha@cdac.in>
2026-06-05 06:11:48 +0000

    logs removed

--- ngap/handler.go
+++ ngap/handler.go
@@ -344,9 +344,6 @@ func FetchRanUeContext(ran *context.AmfRan, message *ngapType.NGAPPDU) (*context
 			}
 			ranUe = context.AMF_Self().RanUeFindByAmfUeNgapID(aMFUENGAPID.Value)
 			ran.Log.Info("UEContextReleaseComplete lookup Amfuengapid:%d", aMFUENGAPID.Value)
-			if ranUe.AmfUe.Supi != "" {
-				ran.Log.Info("Fetched Amfuengaid:%d, Supi:%s, ranue:%p", ranUe.AmfUeNgapId, ranUe.AmfUe.Supi, ranUe)
-			}
 
 		case ngapType.ProcedureCodePDUSessionResourceRelease:
 			ngapMsg := successfulOutcome.Value.PDUSessionResourceReleaseResponse
